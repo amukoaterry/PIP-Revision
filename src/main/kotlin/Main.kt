93445cc1b7905f88f697fa6dd14668a246bd6f6a
amukoaterry <amukoaterrymitchell@gmail>
2024-03-19 21:04:42 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+fun main(){
+    //A variable is a container that stores data temporarily//
+    //var....is mutable//
+    //val...immutable//
+
+    //DATA TYPES//
+    //EG;Numbers---Integer,f;loat,double,byte,long,short
+    //   ;Boolean
+    //   ;Characters
+    var a = 20
+    var b = 10
+    var add = a + b
+    var subtract = a - b
+    var  multiply = a * b
+    var result = a / b
+    var modulus = a % b
+
+
+    println(add)
+    println(subtract)
+    println(multiply)
+    println(result)
+    println(modulus)
+
+
+    var width = 20
+    var length = 10
+    var area = width * length
+
+    println(area)
+
+    var fullName = "Terry Mitchell"
+    var age = 21
+    var phoneNumber = "0748375701"
+    var weight = 75.00
+    var citizen = "Kenyan"
+
+    if(citizen == "Kenyan")
+    {println("True")}
+        else {println("False")}
+
+        println(fullName)
+        println(age)
+        println(phoneNumber)
+        println(weight)
+        println(citizen)
+
+
+
+
+
+
+}
